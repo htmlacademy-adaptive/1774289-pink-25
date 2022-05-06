22ebfca5eb063b9343ba71cb505b98a1514eb269
Zhanna <janna500116@gmail.com>
2022-05-07 00:41:00 +0700

    Адаптив главной страницы

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,16 @@
-import Slider from './blocks/slider.js';
-import PageHeader from './blocks/page-header.js';
 import Add from './blocks/add.js';
 import Form from './blocks/form.js';
+import Map from './blocks/map.js';
+import PageHeader from './blocks/page-header.js';
+import Slider from './blocks/slider.js';
 import Viewport from './blocks/viewport.js';
 import { setupBlocks } from './common/util.js';
 
 [
-  ['.slider', Slider],
-  ['.page-header', PageHeader],
   ['.add', Add],
   ['.form', Form],
+  ['.map', Map],
+  ['.page-header', PageHeader],
+  ['.slider', Slider],
   ['.viewport[id]', Viewport]
 ].forEach(setupBlocks);
